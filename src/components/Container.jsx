2d67776f0eff9c437dac4f1ff15dd0e0b42846fa
Lysenko <lysenkoden@gmail.com>
2023-09-04 08:51:18 +0200

    add logic to reset button

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,17 +1,42 @@
 import React, { useState } from "react";
-import { FaPause, FaPlay } from "react-icons/fa";
+import { ImPlay2, ImPause } from "react-icons/im";
+import { FaRegArrowAltCircleDown, FaRegArrowAltCircleUp } from "react-icons/fa";
 import { FaClockRotateLeft } from "react-icons/fa6";
-import ButtonDown from "./ButtonDown";
-import ButtonUp from "./ButtonUp";
 import Author from "./Author";
 
 const Container = () => {
   const [breaker, setBreaker] = useState(5);
   const [session, setSession] = useState(25);
+  const [timer, setTimer] = useState(formatAsTime);
+  const buttonStyle =
+    "transition-transform duration-[0.3s] ease-[ease-in-out] shadow-[3px_2px_5px_black] hover:shadow-[4px_3px_5px_black] hover:scale-110 hover:bg-[#50644d] active:shadow-[2px_1px_4px_black] active:scale-110 rounded-[50%] h-[35px] w-[35px]";
+
+  const timeResetHandler = () => {
+    setBreaker(5);
+    setSession(25);
+    setTimer(25);
+  };
+
+  //!===================================================
+  var formatAsTime = (session) => {
+    let seconds = session * 10;
+    let minutes = parseInt(seconds / 60);
+    seconds = seconds % 60;
+
+    let displayTime =
+      minutes.toString().padStart(2, "0") +
+      ":" +
+      seconds.toString().padStart(2, "0");
+
+    return displayTime;
+  };
+  console.log(formatAsTime(session)); //
+  //!===================================================
+
   return (
     <div>
       <div id="wrapper" className="p-[5px] max-w-fit">
-        <h1 id="main-title" className="font-extrabold text-2xl">
+        <h1 id="main-title" className="font-extrabold text-[26px]">
           Pomodoro Clock
         </h1>
         <div
@@ -24,13 +49,13 @@ const Container = () => {
             </div>
             <div id="brake-controls" className="flex flex-wrap justify-between">
               <div id="break-decrement" className="">
-                <ButtonDown />
+                <FaRegArrowAltCircleDown className={buttonStyle} />
               </div>
               <div id="break-length" className="text-xl font-bold px-1">
-                {breaker}
+                {breaker < 10 ? breaker.toString().padStart(2, "0") : breaker}
               </div>
               <div id="break-increment" className="">
-                <ButtonUp />
+                <FaRegArrowAltCircleUp className={buttonStyle} />
               </div>
             </div>
           </div>
@@ -43,33 +68,33 @@ const Container = () => {
               className="flex flex-wrap justify-between"
             >
               <div id="session-decrement" className="">
-                <ButtonDown />
+                <FaRegArrowAltCircleDown className={buttonStyle} />
               </div>
               <div id="session-length" className="text-xl font-bold px-1">
-                {session}
+                {session < 10 ? session.toString().padStart(2, "0") : session}
               </div>
               <div id="session-increment" className="">
-                <ButtonUp />
+                <FaRegArrowAltCircleUp className={buttonStyle} />
               </div>
             </div>
           </div>
         </div>
         <div id="timer-wrapper" className="">
-          <div id="timer-label" className="font-extrabold text-xl">
+          <div id="timer-label" className="font-extrabold text-2xl">
             Work it baby!
           </div>
-          <div id="time-left" className="text-6xl font-bold">
+          <div id="time-left" className="text-8xl font-bold">
             25:00
           </div>
         </div>
 
         <div id="controls-wrapper" className="flex justify-around">
           <div id="start_stop" className="">
-            <FaPlay className="transition-transform duration-[0.3s] ease-[ease-in-out] shadow-[3px_2px_5px_black] hover:shadow-[4px_3px_5px_black] hover:scale-110 hover:bg-[#50644d] active:shadow-[2px_1px_4px_black] active:scale-110 h-[25px] w-[25px]" />
-            <FaPause className="transition-transform duration-[0.3s] ease-[ease-in-out] shadow-[3px_2px_5px_black] hover:shadow-[4px_3px_5px_black] hover:scale-110 hover:bg-[#50644d] active:shadow-[2px_1px_4px_black] active:scale-110 h-[25px] w-[25px]" />
+            <ImPlay2 className={buttonStyle} />
+            <ImPause className={buttonStyle} />
           </div>
           <div id="reset" className="">
-            <FaClockRotateLeft className="transition-transform duration-[0.3s] ease-[ease-in-out] shadow-[3px_2px_5px_black] hover:shadow-[4px_3px_5px_black] hover:scale-110 hover:bg-[#50644d] active:shadow-[2px_1px_4px_black] active:scale-110 rounded-[50%] h-[25px] w-[25px]" />
+            <FaClockRotateLeft className={buttonStyle} />
           </div>
         </div>
       </div>
